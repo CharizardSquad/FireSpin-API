7db81f58d3bf270ad3d0fa4b9b0098232e41ece2
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-28 13:24:38 -0700

    boilerplate

---
+++ client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const App = () => {
+  return (
+    <div>
+      "hello"
+
+    </div>
+  );
+};
+export default App;
---
+++ types.ts
@@ -0,0 +1,6 @@
+// import type { RequestHandler } from 'express'
+
+
+export interface ServerError {
+  err: '400'
+}
