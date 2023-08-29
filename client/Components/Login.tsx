8972569d37c274576c8e8a0c4c6a7bafe8e10d01
Blast61 <aderritt6158@gmail.com>
2023-08-29 16:01:28 -0500

    initailized new feature branch

--- client/Components/Login.tsx
+++ client/Components/Login.tsx
@@ -1,13 +1,39 @@
 /* eslint-disable @typescript-eslint/semi */
-import React, { type ReactElement } from 'react'
+import React, { type ReactElement, useState, ChangeEvent, FormEvent } from 'react';
 // import { GoogleLogin } from "google-auth-library";
-function Login (): ReactElement {
-  // const responseGoogle = (response) =>{
-  // console.log(response)
-  // }
+ function Login():ReactElement {
+  const [{username, password}, setSignIn] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    console.log('Form was submitted!');
+  };
+
+
+
+  // const formData = new FormData();
+  // const req = async (): Promise<string> => 
+  // await fetch('/login', {
+  //   method: 'POST',
+  //   body: formData,
+  // });
+  
+  // const resText= req.text();
+  // setSignIn(resText);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSignIn({
+      username: e.target.value,
+      password: e.target.value,
+    });
+  }
+
   return (
     <div>
-      <h2>Sign in with Google</h2>
+      {/* <h2>Sign in with Google</h2> */}
       {/* <GoogleLogin
         clientId="YOUR_CLIENT_ID"
         buttonText="Sign in with Google"
@@ -15,8 +41,16 @@ function Login (): ReactElement {
         onFailure={responseGoogle}
         cookiePolicy="single_host_origin"
       /> */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username</label>
+          <input type="text" id="username" onChange={handleChange} value={username}></input>
+          <label>Password</label>
+          <input type="password" value={password} id="password"></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
+
