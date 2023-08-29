142d1538ca07086138ed0f1a51fc57c3a3406cbd
eishakaushal <eishakaushal@gmail.com>
2023-08-28 21:11:53 -0400

    initialized sequelize instance

---
+++ server/db.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('postgres://sgctvduk:VGmxt4EVA1UbmH97TPJ3G7QuAeQXzm9j@berry.db.elephantsql.com/sgctvduk');
+
+export default sequelize;
