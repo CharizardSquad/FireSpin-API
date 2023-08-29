38f3586c1562a4b25cd25e877556ac3c1f070977
eishakaushal <eishakaushal@gmail.com>
2023-08-28 21:12:59 -0400

    created user model

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,1 +1,18 @@
-import { Sequelize, Model, DataTypes } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db';
+
+class User extends Model {}
+
+User.init({
+  // Model attributes are defined here
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, { sequelize });
+
+export default User;
