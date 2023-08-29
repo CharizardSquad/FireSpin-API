d36f39e89fe65ad854dc9de1e1342f92864faa67
eishakaushal <eishakaushal@gmail.com>
2023-08-28 21:12:18 -0400

    initialized sequelize instance

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,10 @@
 import { Sequelize } from 'sequelize';
+import * as dotenv from 'dotenv';
 
-const sequelize = new Sequelize('postgres://sgctvduk:VGmxt4EVA1UbmH97TPJ3G7QuAeQXzm9j@berry.db.elephantsql.com/sgctvduk');
+dotenv.config({ path: __dirname+'/.env' });
+
+const sequelize = new Sequelize(process.env.DB_URI ?? '', {
+  dialect: 'postgres' // Specify PostgreSQL dialect
+});
 
 export default sequelize;
