906bd53562b4be1b9749e5df7769f5c493349411
eishakaushal <eishakaushal@gmail.com>
2023-08-28 21:13:21 -0400

    created response time model

---
+++ server/models/ResponseTime.ts
@@ -0,0 +1,14 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db';
+
+class ResponseTime extends Model {}
+
+ResponseTime.init({
+  // Model attributes are defined here
+  time: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, { sequelize });
+
+export default ResponseTime;
