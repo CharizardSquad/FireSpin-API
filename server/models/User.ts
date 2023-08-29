ea06f9014a18caa293c2b28057c499c1fdd4607f
eishakaushal <eishakaushal@gmail.com>
2023-08-29 04:10:22 -0400

    added unique attribute to User model

--- server/models/User.ts
+++ server/models/User.ts
@@ -11,7 +11,8 @@ User.init({
   // Model attributes are defined here
   username: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   password: {
     type: DataTypes.STRING,
