023a6a0d1f9f6c132e096fb214269d75aff67eea
Blast61 <aderritt6158@gmail.com>
2023-08-29 16:29:42 -0500

    Login functionality implemented and working

--- client/Components/Login.tsx
+++ client/Components/Login.tsx
@@ -7,29 +7,46 @@ import React, { type ReactElement, useState, ChangeEvent, FormEvent } from 'reac
     password: ''
   });
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) =>{
-    event.preventDefault();
-    console.log('Form was submitted!');
-  };
-
-
-
+  
+  
+  
   // const formData = new FormData();
   // const req = async (): Promise<string> => 
   // await fetch('/login', {
-  //   method: 'POST',
-  //   body: formData,
-  // });
+    //   method: 'POST',
+    //   body: formData,
+    // });
+    
+    // const resText= req.text();
+    // setSignIn(resText);
+    let formData;
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setSignIn((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    };
+    
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) =>{
+      event.preventDefault();
+      console.log('Form was submitted!');
+      try{
+        const response = await fetch('api/login', {
+          method: 'POST',
+          headers: {"Content-Type": "application/json",},
+          body: JSON.stringify({ username, password })
+        })
+        if(response.ok){
+          console.log('Form submitted successfully')
+        } else{
+          console.error('Error submitting form.')
+        }
   
-  // const resText= req.text();
-  // setSignIn(resText);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSignIn({
-      username: e.target.value,
-      password: e.target.value,
-    });
-  }
+      } catch(err){
+        console.error('An error occurred:', err)
+      }
+    };
 
   return (
     <div>
@@ -43,9 +60,9 @@ import React, { type ReactElement, useState, ChangeEvent, FormEvent } from 'reac
       /> */}
       <form onSubmit={handleSubmit}>
         <label htmlFor='username'>Username</label>
-          <input type="text" id="username" onChange={handleChange} value={username}></input>
+          <input type="text" id="username" onChange={handleChange} value={username} name="username"></input>
           <label>Password</label>
-          <input type="password" value={password} id="password"></input>
+          <input type="password" value={password} id="password" onChange={handleChange} name="password"></input>
         <button type="submit">Submit</button>
       </form>
     </div>
