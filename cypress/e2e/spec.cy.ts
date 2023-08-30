a3a38488b77ced442943db40bea7a811251fb023
eishakaushal <eishakaushal@gmail.com>
2023-08-30 17:47:17 -0400

    added cypress e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,19 +1,27 @@
 describe('Signup Test', () => {
   it('passes', () => {
-    cy.visit('/')
+    cy.visit('/');
 
-    cy.get('#username').type('username');
-    cy.get('#password').type('password');
+    cy.get('#username').type('testing');
+    cy.get('#password').type('testing');
 
     // Submit the form
     cy.contains('Submit').click();
-    cy.url().should('include', 'home')
+    cy.url().should('include', 'home');
 
-    // cy.get('#username').type('username');
-    // cy.get('#password').type('password');
+    const pastedText = 'https://dog.ceo/api/breeds/image/random';
 
-    // // Submit the form
-    // cy.contains('Submit').click();
+    cy.get('.input-API').type(pastedText);
+    cy.get('call-freq').type('2');
+    cy.contains('Fetch Data').click();
+    cy.get('.lineGraph').should('exist');
 
-  })
-})
+    cy.contains('History').click();
+    cy.get('.modal-content').should('exist');
+    cy.get('.modal-content').within(() => {
+      cy.contains(pastedText).should('exist');
+    });
+
+    });
+});
+
